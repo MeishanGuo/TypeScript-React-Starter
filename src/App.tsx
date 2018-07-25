7d502394e5000bc2e17a44e5fd34e72cd0c100a6
郭美山 <guomeishan@guomeishandeMBP.lan>
2018-07-25 22:41:16 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          董婉婷是傻狗！
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
